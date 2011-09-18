c6da35e4cdc355c3409dc89f48513e000684e14d
Anay Nayak <anayak@thoughtworks.com>
2011-09-18 20:56:31 +0530

    AWS security groups can now be visualized! Added capability to represent multiple ip addresses under the same group

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+
+gem 'ruby-graphviz'
+gem 'right_aws'
---
+++ groups.rb
@@ -0,0 +1,5 @@
+USER_GROUPS = {
+  '0.0.0.0/0' => '*',
+#  'x.x.x.x/0' => 'External',
+#  'y.y.y.y/0' => 'External',
+}
