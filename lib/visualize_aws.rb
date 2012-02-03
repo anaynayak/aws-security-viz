59dbf83329e0f746d7bd5b541cef80fe1359b54c
Anay <anayak007@gmail.com>
2012-02-03 18:59:24 +0530

    First spec! Still need to work on it

--- lib/visualize_aws.rb
+++ lib/visualize_aws.rb
@@ -7,22 +7,24 @@ class VisualizeAws
   def initialize(access_key, secret_key)
     @ec2 = RightAws::Ec2.new(access_key, secret_key)
   end
-  def unleash
+  def parse
     groups = @ec2.describe_security_groups
-    p groups
-    nodes = groups.collect {|group| group[:aws_group_name]}
-
     g = GraphViz::new( "G" )
+    nodes = groups.collect {|group| group[:aws_group_name]}
     nodes.each {|n| g.add_node(n)}
     identify_group_ingress(groups) {|from, to, port_range| g.add_edge( from, to, :color => "blue", :style => "bold", :label => port_range )}
     identify_cidr_ingress(groups) {|from, to, port_range| g.add_edge( from, to, :color => "green", :style => "bold", :label => port_range )}
+    g
+  end
+  def unleash
+    g = parse
     g.output( :png => "aws-security-viz.png" )
   end
 
   def identify_group_ingress(groups)
     groups.each do |group|
       group[:aws_perms].each do |perm|
-        yield perm[:group], group[:aws_group_name], [perm[:from_port], perm[:to_port]].uniq.join("-")  if perm[:group]
+        yield perm[:group_name], group[:aws_group_name], [perm[:from_port], perm[:to_port]].uniq.join("-")  if perm[:group_name]
       end
     end
   end
--- spec/visualize_aws_spec.rb
+++ spec/visualize_aws_spec.rb
@@ -1,7 +1,35 @@
 require 'spec_helper'
 
 describe VisualizeAws do 
-  it "should identify mapping" do
-    
+  before do
+    @ec2 = mock(RightAws::Ec2)
+    RightAws::Ec2.should_receive(:new).and_return(@ec2)
+    @visualize_aws = VisualizeAws.new "key", "secret"
+  end
+
+  it "should add nodes for each security group" do
+    @ec2.should_receive(:describe_security_groups).and_return(sample)
+    graph = @visualize_aws.parse()
+    node1 = graph.get_node('Remote ssh')
+    node2 = graph.get_node('mymachine') 
+    node1.should_not be_nil
+    node2.should_not be_nil
+    graph.each_edge.size.should == 1
+    edge = graph.each_edge.first
+    edge.node_one.should == GraphViz.escape(node2.id)
+    edge.node_two.should == GraphViz.escape(node1.id)
+  end
+
+  def sample
+    [
+      {
+        :aws_owner=>"aws-owner", :aws_group_name=>"Remote ssh", :aws_description=>"Ssh into remote box", :group_id=>"sg-111", 
+        :aws_perms=>[{:from_port=>"22", :to_port=>"22", :protocol=>"tcp", :direction=>:ingress, :group_name=>"mymachine", :group_id=>"sg-123", :owner=>"own-123"}]
+      },
+      {
+        :aws_owner=>"aws-owner", :aws_group_name=>"mymachine", :aws_description=>"my_machine", :group_id=>"sg-222", 
+        :aws_perms=>[]
+      }
+    ]
   end
 end
