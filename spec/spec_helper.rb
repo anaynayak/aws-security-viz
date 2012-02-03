83e0341134bd74346f9351b36d10bd4b974f1135
Anay <anayak007@gmail.com>
2012-02-03 20:44:50 +0530

    Simplified data setup. Tests now read better

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,14 @@ require 'bundler'
 Bundler.require
 
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+
+def group name, *ingress
+  {
+    :aws_owner=>"aws-owner", :aws_group_name=> name, :aws_description=>"Ssh into remote box", :group_id=>"sg-111", 
+    :aws_perms=> ingress
+  }
+end
+
+def ingress port, name
+  {:from_port=>port, :to_port=>port, :protocol=>"tcp", :direction=>:ingress, :group_name=>name, :group_id=>"sg-123", :owner=>"own-123"}
+end
