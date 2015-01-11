2a5346bfaa46409b23eef2d73eea3a497241cfb2
Anay Nayak <anayak@thoughtworks.com>
2015-01-11 17:20:07 +0530

    Added integration spec

---
+++ spec/integration/visualize_aws_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe VisualizeAws do
+  context 'json' do
+    let(:opts) {
+      {
+        :source_file => source_file,
+        :filename => temp_file
+      }
+    }
+    let(:source_file) {File.join(File.dirname(__FILE__), 'dummy.json')}
+    let(:expected_file) {File.join(File.dirname(__FILE__), 'dummy.dot')}
+    let(:temp_file) { Tempfile.new(%w(aws .dot)) }
+
+    it 'should parse json input' do
+      VisualizeAws.new(opts).unleash(temp_file.path)
+
+      expect(File.read(expected_file)).to eq(temp_file.read)
+    end
+  end
+end
